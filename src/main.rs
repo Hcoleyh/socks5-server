106f7f40669ce662cc6907545292fee7d23e5174
Hcoleyh <hcoleyh@gmail.com>
2022-02-17 17:16:40 +0800

    Add cli args parser

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,40 @@ use tokio;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    socks5_server::run("127.0.0.1:1080").await
+    match parse_args() {
+        Ok((addr, port)) => 
+            socks5_server::run(&format!("{}:{}", addr, port)).await,
+        _ => {
+            help();
+            Ok(())
+        }
+    }
+}
+
+fn parse_args() -> Result<(String, String), lexopt::Error> {
+    use lexopt::prelude::*;
+
+    let mut addr = String::from("127.0.0.1");
+    let mut port = String::from("1080");
+
+    let mut parser = lexopt::Parser::from_env();
+    while let Some(arg) = parser.next()? {
+        match arg {
+            Short('b') => {
+                addr = parser.value()?.into_string()?;
+            },
+            Short('p') => {
+                port = parser.value()?.into_string()?;
+            },
+            Long("help") => help(),
+            _ => help()
+        }
+    }
+
+    Ok((addr, port))
+}
+
+fn help() {
+    println!("Usage: socks5_server [-b BIND_ADDR] [-p PORT]");
+    std::process::exit(0);
 }
